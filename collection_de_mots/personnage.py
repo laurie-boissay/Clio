04e3d64ea9d7c8f6aa16bf1669ae435233d81cfa
Jaenne <59830376+laurie-boissay@users.noreply.github.com>
2020-06-10 14:15:39 +0200

    Add files via upload

---
+++ collection_de_mots/personnage.py
@@ -0,0 +1,407 @@
+classes_et_carac_associee = {
+
+	"archer/archère" : "Dextérité",
+	"archère" : "Dextérité",
+	"archer" : "Dextérité",
+	"assassin" : "Dextérité",
+	"barbare" : "Force",
+	"barde" : "Charisme",
+	"druide" : "Sagesse",
+	"guerrière/guerrier" : "Force",
+	"guerrier" : "Force",
+	"guerrière" : "Force",
+	"mage" : "Intelligence",
+	"moine" : "Sagesse",
+	"nécromancienne" : "Intelligence",
+	"nécromancien" : "Intelligence",
+	"nécromancien.ne" : "Intelligence",
+	"paladin" : "Charisme",
+	"paladin.e" : "Charisme",
+	"paladine" : "Charisme",
+	"prêtre" : "Sagesse",
+	"prêtresse" : "Sagesse",
+	"prêtre.sse" : "Sagesse",
+	"rodeuse" : "Dextérité",
+	"rodeur" : "Dextérité",
+	"rodeur/rodeuse" : "Dextérité",
+	"sorcier/sorcière" : "Intelligence",
+	"sorcier" : "Intelligence",
+	"sorcière" : "Intelligence",
+	"voleur/voleuse" : "Dextérité",
+	"voleur" : "Dextérité",
+	"voleuse" : "Dextérité",
+	}
+
+classe = [
+	"archer/archère", # 0
+	"assassin", # 1
+	"barbare", # 2
+	"barde", # 3
+	"druide", # 4
+	"guerrière/guerrier", # 5
+	"mage", # 6
+	"moine", # 7
+	"nécromancien.ne", # 8
+	"paladin.e", # 9
+	"prêtre.sse", # 10
+	"rodeur/rodeuse", # 11
+	"sorcier/sorcière", # 12
+	"voleur/voleuse", # 13
+	]
+
+classe_f = [
+	"archère", # 0
+	"assassin", # 1
+	"barbare", # 2
+	"barde", # 3
+	"druide", # 4
+	"guerrière", # 5
+	"mage", # 6
+	"moine", # 7
+	"nécromancienne", # 8
+	"paladine", # 9
+	"prêtresse", # 10
+	"rodeuse", # 11
+	"sorcière", # 12
+	"voleuse", # 13
+	]
+
+classe_m = [
+	"archer", # 0
+	"assassin", # 1
+	"barbare", # 2
+	"barde", # 3
+	"druide", # 4
+	"guerrier", # 5
+	"mage", # 6
+	"moine", # 7
+	"nécromancien", # 8
+	"paladin", # 9
+	"prêtre", # 10
+	"rodeur", # 11
+	"sorcier", # 12
+	"voleur", # 13
+	]
+
+carac = [
+	"Force",
+	"Dextérité",
+	"Constitution",
+	"Intelligence",
+	"Sagesse",
+	"Charisme",
+	]
+
+genre = [
+	"féminin",
+	"masculin",
+	"androgyne"
+	]
+
+prenoms_f = [
+	"An",
+	"Annia",
+	"Alaina",
+	"Aminata",
+	"Aïssata",
+	"Aya",
+	"Berthille",
+	"Bérénia",
+	"Fatou",
+	"Floraline",
+	"Gadrielle",
+	"Jocynthe",
+	"Kéline",
+	"Koralie",
+	"Liya",
+	"Linh",
+	"Malaïka",
+	"Marille",
+	"Mytrinne",	
+	"Naelwen",
+	"Natasha",
+	"Ngoc",
+	"Odaelle",
+	"Oumou",
+	"Prescille",
+	"Shana"
+	"Sorsha",
+	"Télinne",
+	"Véronnelle",
+	"Wendoline",
+
+	"Astrid",
+	"Arnhild",
+	"Berthild",
+	"Brynild",
+	"Eldrid",
+	"Ermenhild",
+	"Frida",
+	"Gerda",
+	"Grimhild",
+	"Gudrun",
+	"Helga",
+	"Hilda",
+	"Ingrid",
+	"Klothild",
+	"Sigrid",
+	"Strida",
+	"Diesa",
+	
+	"Elberenh",
+	"Elidhwen",
+	"Ellenùviel",
+	"Laurelinn",
+	"Linaëwen",
+	"Laendorië",
+	"Maelwë",
+	"Maerwen",
+	"Nennenvaël",
+	"Ninwelotë",
+	"Tintaëlle",
+	"Aëldill",
+	"Aluinill",
+	"Aliann",
+	"Dianaë",
+	"Eilinelle",
+	"Elenwëe",
+	"Lùthill",
+	"Ninielle",
+	"Nolwaënn",
+	"Mirielle",
+	"Keltienn",
+	
+	"Aoga",
+	"Bargula",
+	"Caroka",
+	"Droga",
+	"Eroka",
+	"Farga",
+	"Goroga",
+	"Hurla",
+	"Jorgula",
+	"Kroka",
+	"Krusha",
+	"Luga",
+	"Moga",
+	"Noroka",
+	"Roka",
+	"Urdua",
+	"Baggi",
+	"Emen",
+	"Engong",
+	"Myev",
+	"Neega",
+	"Ovak",
+	"Ownka",
+	"Shautha",
+	"Vola",
+	"Volen",
+	
+	"Eglantine",
+	"Lila",
+	"Jacinthe",
+	"Rose",
+	"Margueritte",
+	"Muguette",
+	"Pâquerette",
+	"Prune",
+	"Jasmine",
+	"Cerise",
+	
+	"Thérèse",
+	"Huguette",
+	"Rose",
+	"Marceline",
+	"Jeannette",
+	"Eugénie",
+	"Denise",
+	"Germaine",
+	"Charlotte",
+	"Gyslaine",
+	]
+
+prenoms_m = [
+	"Adalrik",
+	"Arn",
+	"Bernulf",
+	"Brand",
+	"Edwald",
+	"Ferwin",
+	"Gerulf",
+	"Godfred",
+	"Gunter",
+	"Habib",
+	"Halfdan",
+	"Ingvar",
+	"Jin",
+	"Keenan",
+	"Ketil",
+	"Knud",
+	"Lassana",
+	"Lothar",
+	"Madhi",
+	"Osvald",
+	"Roderick",
+	"Rurik",
+	"Sékou",
+	"Sigfred",
+	"Sigmar",
+	"Sigvald",
+	"Souleymane",
+	"Stig",
+	"Svenn",
+	"Tao",
+	"Thorsten",
+	"Yu",
+	
+	"Gloin",
+	"Krorin",
+	"Thorin",
+	"Buldur",
+	"Trorin",
+	"Orik",
+	"Durok",
+	"Guerann",
+	"Korik",
+	"Barrend",
+	"Brottor",
+	
+	"Arwendil",
+	"Caëldwendir",
+	"Eldwyndor",
+	"Elberenhdir",
+	"Elendur",
+	"Gilgalendil",
+	"Irwildur",
+	"Laurelith",
+	"Linaëndir",
+	"Laendoril",
+	"Nennendir",
+	"Aëdin",
+	"Caëndor",
+	"Cirion",
+	"Doralion",
+	"Ealdor",
+	"Findalion",
+	"Glorfindor",
+	"Haëldion",
+	"Laurendor",
+	"Morwendir",
+	"Raëldirion",
+	
+	"Aog",
+	"Bargul",
+	"Carok",
+	"Drog",
+	"Erok",
+	"Farg",
+	"Gorog",
+	"Hurl",
+	"Jorgul",
+	"Krok",
+	"Krush",
+	"Lug",
+	"Mog",
+	"Norok",
+	"Rok",
+	"Urdu",
+	"Dench",
+	"Feng",
+	"Gell",
+	"Henk",
+	"Holg",
+	"Imsh",
+	"Keth",
+	"Krusk",
+	"Ront",
+	"Shump",
+	"Thokk",
+	
+	"Bill",
+	"Bern",
+	"Don",
+	"Duky",
+	"Eckel",
+	"Fili",
+	"Ged",
+	"Gerry",
+	"Hek",
+	"Hicks",
+	"Jack",
+	"Karl",
+	"Litle",
+	"Luky",
+	"Mike",
+	"Polo",
+	"Sam",
+	"Titi",
+
+	"Albert",
+	"Edgard",
+	"Eustache",
+	"Firmin",
+	"Isidore",
+	"Nestor",
+	"Charles",
+	"Bertrand",
+	"Louis",
+	"Guy",
+	"Guilbert",
+	]
+
+prenoms_a = [
+	"An",
+	"Ngoc",
+	"Oumou",
+	"Arnhild",
+	"Brynild",
+	"Eldrid",
+	"Ermenhild",
+	"Grimhild",
+	"Gudrun",
+	"Sigrid",
+	"Elberenh",
+	"Elidhwen",
+	"Ellenùviel",
+	"Maerwen",
+	"Nennenvaël",
+	"Ninwelotë",
+	"Aëldill",
+	"Aluinill",
+	"Aliann",
+	"Lùthill",
+	"Nolwaënn",
+	"Keltienn",
+	"Baggi",
+	"Emen",
+	"Engong",
+	"Myev",
+	"Neega",
+	"Ovak",
+	"Volen",
+	"Bernulf",
+	"Ferwin",
+	"Jin",
+	"Ketil",
+	"Lassana",
+	"Sékou",
+	"Arwendil",
+	"Caëldwendir",
+	"Elberenhdir",
+	"Elendur",
+	"Laurelith",
+	"Linaëndir",
+	"Aëdin",
+	"Farg",
+	"Lug",
+	"Urdu",
+	"Dench",
+	"Feng",
+	"Gell",
+	"Bern",
+	"Eckel",
+	"Litle",
+	"Luky",
+	"Sam",
+	]
