6ffaf73f7b6b6074a8bc0ad1b620903f914daa2d
Jaenne <59830376+laurie-boissay@users.noreply.github.com>
2020-06-12 19:20:00 +0200

    Add files via upload

--- collection_de_mots/equipement.py
+++ collection_de_mots/equipement.py
@@ -5,61 +5,61 @@ Listes des équipements.
 armes_armures = [
 	
 	{# armes à une main
-	"hache" : [[1, "d6 deg, sur la ", "force"], "à une main, au corps à corps"],
-	"épée" : [[1, "d6 deg, sur la ", "force"], "à une main, au corps à corps"],
+	"hache" : [[1, "d6 deg, touche sur la ", "force"], "à une main", "au corps à corps", 25],
+	"épée" : [[1, "d6 deg, touche sur la ", "force"], "à une main", "au corps à corps", 25],
 
-	"bouclier" : [[1, " en ", "défense"], "à une main, comme armure"],
+	"bouclier" : [[1, " en ", "défense"], "à une main", "comme armure", 25],
 
-	"dague" : [[1, "d6 deg, sur la ", "dextérité"], "à une main, au corps à corps"],
+	"dague" : [[1, "d6 deg, touche sur la ", "dextérité"], "à une main", "au corps à corps", 25],
 
-	"poignard sacrificiel" : [[1, "d6 deg, sur l\'", "intelligence"], "à une main, au corps à corps"],
+	"poignard sacrificiel" : [[1, "d6 deg, touche sur l\'", "intelligence"], "à une main", "au corps à corps", 25],
 
-	"croix massive en argent" : [[1, "d6 deg, sur la ", "sagesse"], "à une main, au corps à corps"],
-	"griffe" : [[1, "d6 deg, sur la ", "sagesse"], "à une main, au corps à corps"],
+	"croix massive en argent" : [[1, "d6 deg, touche sur la ", "sagesse"], "à une main", "au corps à corps", 25],
+	"griffe" : [[1, "d6 deg, touche sur la ", "sagesse"], "à une main", "au corps à corps", 25],
 
-	"rapière" : [[1, "d6 deg, sur le ", "charisme"], "à une main, au corps à corps"],
-	"morgenstern" : [[1, "d6 deg, sur le ", "charisme"], "à une main, au corps à corps"],
+	"rapière" : [[1, "d6 deg, touche sur le ", "charisme"], "à une main", "au corps à corps", 25],
+	"morgenstern" : [[1, "d6 deg, touche sur le ", "charisme"], "à une main", "au corps à corps", 25],
 
 	
-	"hachettes de lancé" : [[1, "d6 deg, sur la ", "force"], "à une main, à distance"],
+	"hachettes de lancé" : [[1, "d6 deg, touche sur la ", "force"], "à une main", "à distance", 25],
 
-	"fronde" : [[1, "d6 deg, sur la ", "dextérité"], "à une main, à distance"],
-	"shuriken" : [[1, "d6 deg, sur la ", "dextérité"], "à une main, à distance"],
+	"fronde" : [[1, "d6 deg, touche sur la ", "dextérité"], "à une main", "à distance", 25],
+	"shuriken" : [[1, "d6 deg, touche sur la ", "dextérité"], "à une main", "à distance", 25],
 
-	"livre de sortilèges" : [[1, "d6 deg, sur l\'", "intelligence"], "à une main, à distance"],
+	"livre de sortilèges" : [[1, "d6 deg, touche sur l\'", "intelligence"], "à une main", "à distance", 25],
 
-	"livre de prières" : [[1, "d6 deg, sur la ", "sagesse"], "à une main, à distance"],
+	"livre de prières" : [[1, "d6 deg, touche sur la ", "sagesse"], "à une main", "à distance", 25],
 
-	"livre de chants" : [[1, "d6 deg, sur le ", "charisme"], "à une main, à distance"],
+	"livre de chants" : [[1, "d6 deg, touche sur le ", "charisme"], "à une main", "à distance", 25],
 	},
 
 	{# armes à deux mains
-	"épée lourde" : [[2, "d6 deg, sur la ", "force"], "à deux mains, au corps à corps"],
-	"hache de guerre" : [[2, "d6 deg, sur la ", "force"], "à deux mains, au corps à corps"],
+	"épée lourde" : [[2, "d6 deg, touche sur la ", "force"], "à deux mains", "au corps à corps", 50],
+	"hache de guerre" : [[2, "d6 deg, touche sur la ", "force"], "à deux mains", "au corps à corps", 50],
 
-	"Katana" : [[2, "d6 deg, sur la ", "dextérité"], "à deux mains, au corps à corps"],
-	"arbalète" : [[2, "d6 deg, sur la ", "dextérité"], "à deux mains, à distance"],
-	"arc" : [[2, "d6 deg, sur la ", "dextérité"], "à deux mains, à distance"],
+	"katana" : [[2, "d6 deg, touche sur la ", "dextérité"], "à deux mains", "au corps à corps", 50],
+	"arbalète" : [[2, "d6 deg, touche sur la ", "dextérité"], "à deux mains", "à distance", 50],
+	"arc" : [[2, "d6 deg, touche sur la ", "dextérité"], "à deux mains", "à distance", 50],
 
-	"sceptre" : [[2, "d6 deg, sur l\'", "intelligence"], "à deux mains, à distance"],
+	"sceptre" : [[2, "d6 deg, touche sur l\'", "intelligence"], "à deux mains", "à distance", 50],
 	
-	"marteau sain" : [[2, "d6 deg, sur la ", "sagesse"], "à deux mains, au corps à corps"],
-	"épée sacrée" : [[2, "d6 deg, sur la ", "sagesse"], "à deux mains, au corps à corps"],
-	"baton" : [[2, "d6 deg, sur la ", "sagesse"], "à deux mains, au corps à corps"],
+	"marteau sain" : [[2, "d6 deg, touche sur la ", "sagesse"], "à deux mains", "au corps à corps", 50],
+	"épée sacrée" : [[2, "d6 deg, touche sur la ", "sagesse"], "à deux mains", "au corps à corps", 50],
+	"baton" : [[2, "d6 deg, touche sur la ", "sagesse"], "à deux mains", "au corps à corps", 50],
 
-	"marteau scintillant" : [[2, "d6 deg, sur le ", "charisme"], "à deux mains, au corps à corps"],
-	"épée rutillante" : [[2, "d6 deg, sur le ", "charisme"], "à deux mains, au corps à corps"],
+	"marteau scintillant" : [[2, "d6 deg, touche sur le ", "charisme"], "à deux mains", "au corps à corps", 50],
+	"épée rutillante" : [[2, "d6 deg, touche sur le ", "charisme"], "à deux mains", "au corps à corps", 50],
 
-	"Lyre" : [[2, "d6 deg, sur le ", "charisme"], "à deux mains, à distance"],
+	"lyre" : [[2, "d6 deg, touche sur le ", "charisme"], "à deux mains", "à distance", 50],
 	},
 
 	{# comme armures
-	"armure de plate" : [[2, " en ", "défense"], [-1, " en ", "dextérité"], "comme armure"],
-	"armure de cuir" : [[1, " en ", "défense"], "comme armure"],
-	"harnais souple" : [[1, " en ", "défense"], "comme armure"],
-	"robe magique" : [[1, " en ", "défense"], "comme armure"],
-	"vêtements élégants" : [[1, " en ", "défense"], "comme armure"],
-	"robe de bure" : [[1, " en ", "défense"], "comme armure"],
-	"pagne" : [[1, " en ", "défense"], "comme armure"],
+	"armure de plate" : [[2, " en ", "défense"], [-1, " en ", "dextérité"], "équipée", "comme armure", 40],
+	"armure de cuir" : [[1, " en ", "défense"], "équipée", "comme armure", 40],
+	"harnais souple" : [[1, " en ", "défense"], "équipé", "comme armure", 40],
+	"robe magique" : [[1, " en ", "défense"], "équipée", "comme armure", 40],
+	"vêtements élégants" : [[1, " en ", "défense"], "équipés", "comme armure", 40],
+	"robe de bure" : [[1, " en ", "défense"], "équipée", "comme armure", 40],
+	"pagne" : [[1, " en ", "défense"], "équipé", "comme armure", 40],
 	},
 ]
