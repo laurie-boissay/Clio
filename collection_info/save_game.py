21c2037ef1ebb21a4f4b06e1bf097967413d4f04
Jaenne <59830376+laurie-boissay@users.noreply.github.com>
2020-06-12 19:20:09 +0200

    Add files via upload

--- collection_info/save_game.py
+++ collection_info/save_game.py
@@ -7,44 +7,40 @@ info_de_partie = [] # prévisions
 """
 info_de_partie = [
 	{
-	nom_joueur_id : ["nom_perso",3,4,5,0,1,2, druide],
-	nom_joueur_id : ["nom_perso",1,0,2,4,3,5, mage]
-	nom_joueur_id : ["nom_perso",0,5,3,4,1,2, moine]
-	"scénario" : 1,
-	"avancement" : 3,													<= Pour la team n° 0
-	"étapes nécessaires" : 5,
-	"inocents tués" : 2,
-	"quête en cours" : "5,3,12,4",	<= code identification quête.
-	"avancement quête" : 2,
-	"étapes quêtes nécessaires" : 3,
-	"code de sauvegarde" : "54,1,14/1,5,78/9"	<= code a entrer en cas de perte de données.
+	allowed_channel : channel.name,
+
+	achats_autorisés : [nom_joueur_id, nom_joueur_id],					<= Pour la team n° 0 (implémenté)
+	
+	combat_autorisés : [nom_joueur_id, nom_joueur_id],
+
+	nom_joueur_id : ["nom_perso",F,Co,D,I,S,Ch,def,PV_max,PV,PA,xp,classe,genre,don,[sac],[équipé]],
+	nom_joueur_id : ["nom_perso",F,Co,D,I,S,Ch,def,PV_max,PV,PA,xp,classe,genre,don,[sac],[équipé]],
+	nom_joueur_id : ["nom_perso",F,Co,D,I,S,Ch,def,PV_max,PV,PA,xp,classe,genre,don,[sac],[équipé]],
+
 	}
 
 	{
-	nom_joueur_id : ["nom_perso",3,4,5,0,1,2, druide],
-	nom_joueur_id : ["nom_perso",1,0,2,4,3,5, mage]
-	nom_joueur_id : ["nom_perso",0,5,3,4,1,2, moine]
+	allowed_channel : channel.name,
+
+	achats_autorisés : [nom_joueur_id, nom_joueur_id],
+	
+	combat_autorisés : [nom_joueur_id, nom_joueur_id],
+
+	nom_joueur_id : ["nom_perso",F,Co,D,I,S,Ch,def,PV_max,PV,PA,xp,classe,genre,don,[sac],[équipé]],
+	nom_joueur_id : ["nom_perso",F,Co,D,I,S,Ch,def,PV_max,PV,PA,xp,classe,genre,don,[sac],[équipé]],
+	nom_joueur_id : ["nom_perso",F,Co,D,I,S,Ch,def,PV_max,PV,PA,xp,classe,genre,don,[sac],[équipé]],
+
 	"scénario" : 1,
-	"avancement" : 3,													<= Pour la team n° 1
+	"avancement" : 3,													<= Pour la team n° 1 (prévisions)
 	"étapes nécessaires" : 5,
 	"inocents tués" : 2,
-	"quête en cours" : "5,3,12,4",	
+	"quête en cours" : "5,3,12,4",
 	"avancement quête" : 2,
 	"étapes quêtes nécessaires" : 3,
 	"code de sauvegarde" : "54,1,14/1,5,78/9"
 	}
 ]
 """
-
-allowed_channels = []
-"""
-allowed_channels = [
-	id_channel_allowed_1,
-	id_channel_allowed_2,
-	id_channel_allowed_3,
-]
-"""
-
 
 team_des_joueurs = {}
 """
