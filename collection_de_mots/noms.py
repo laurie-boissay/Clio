3c1dac258d4567bab6e62a68cc56f2e889b98d2a
Jaenne <59830376+laurie-boissay@users.noreply.github.com>
2020-06-16 18:47:38 +0200

    Add files via upload

---
+++ collection_de_mots/noms.py
@@ -0,0 +1,174 @@
+"""
+Les listes qui permettent de générer des noms sauf les noms et prénoms de personnage.
+
+"""
+
+auberge_nom = [
+	"Le sanglier",
+	"Le cerf",
+	"les trois p'tits chats",
+	"La caille",
+	"le porc",
+	"l'ours",
+	"l'enfant",
+	"les marrons",
+	"la citrouille",
+	"la marraine",
+	"l'étoile",
+	"les poissons",
+	"le pêcheur",
+	"la chasseuse",
+	"les oies",
+	"les pommes de terres",
+	"le boeuf",
+	"la vache",
+	"les faisans",
+	"le merlu",
+	"le chevreuil",
+	"la chèvre",
+	"les lapins",
+	"le canard",
+	"le fromage",
+	"le festin",
+	"la cuisse",
+	"le mouton",
+	"l'agneau",
+	"le gigot",
+	"le ragout",
+	"la tourte",
+	]
+
+auberge_adj = [
+	"rieur/rieuse.s",
+	"chanceux/chanceuse.s",
+	"colérique.s",
+	"du roi",
+	"de la reine",
+	"gourmand.e.s",
+	"joyeux/joyeuse.s",
+	"rouge.s",
+	"gentil.le.s",
+	"chaud.e.s",
+	"roux/rousse.s",
+	"aillé.e.s",
+	"roti.e.s",
+	"gras.se.s",
+	"faisandé.e.s",
+	"barbu.e.s",
+	"cendré.e.s",
+	"fumé.e.s",
+	"gargantuesque.s",
+	"doré.e.s",
+	"qui sent",
+	"goutu.e.s",
+	"aux pruneaux",
+	"juteux/justeuse.s",
+	"à l'ail",
+	"piquant.e.s",
+	"épicé.e.s",
+	"crémeux/crémeuse.s",
+	]
+
+navire_nom = [
+	"l'anguille",
+	"la sirène",
+	"le loup",
+	"le lamentin",
+	"le sabre",
+	"le capitaine",
+	"l'étoile",
+	"le poisson",
+	"l'équipage",
+	"la sorcière",
+	]
+
+navire_adj = [
+	"des mers",
+	"intrépide",
+	"chanceux/chanceuse",
+	"sans peurs",
+	"libre.s",
+	"fier/fière",
+	"valeureux/valeureuse",
+	"volant.e",
+	"sacré.e",
+	"vert.e",
+	"étincelant.e",
+	]
+
+lieu_quete_nom = [
+	"le temple",
+	"la tombe",
+	"les cavernes",
+	"le chateau",
+	"la pyramide",
+	"le couvent",
+	"l'ancienne prison",
+	"le donjon",
+	"le labyrinthe",
+	"l'antre",
+	]
+
+lieu_quete_adj = [
+	"du roi maudit",
+	"la reine rouge",
+	"sombre.s",
+	"hanté.e.s",
+	"maudit.e.s",
+	"du dragon blanc",
+	]
+
+ext_nom = [
+	"la forêt",
+	"la montagne",
+	"l'île",
+	"les plaines",
+	"la vallée",
+	"la jungle",
+	"le verger",
+	]
+
+ext_adj = [
+	"sombre.s",
+	"sacré.e.s",
+	"maudit.e.s",
+	"vert.e.s",
+	]
+
+objet_nom = [
+	"l'épée",
+	"la lance",
+	"le marteau",
+	"la targe",
+	"le calice",
+	"le livre",
+	"la clée",
+	"le heaume",
+	"la couronne",
+	"l'orbe",
+	"le sceptre",
+	"la rose",
+	"le miroir",
+	"l'arc",
+	"le prisme",
+	]
+
+objet_adj = [
+	"sacré.e",
+	"divin.e",
+	"des dieux",
+	"saint.e",
+	"étincelent.e",
+	"de lumière",
+	"du juste",
+	"des géants",
+	"de vérité",
+	"de justice",
+	"de courage",
+	"vaillant.e",
+	"de colère",
+	"de puissance",
+	"de raison",
+	"du sage",
+	"du mage blanc",
+	]
