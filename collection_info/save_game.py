902ba3f45b2266fef9422cb97961376ff7cc4cbf
Jaenne <59830376+laurie-boissay@users.noreply.github.com>
2020-06-17 22:28:29 +0200

    Add files via upload

--- collection_info/save_game.py
+++ collection_info/save_game.py
@@ -3,21 +3,53 @@ Ces listes et dictionnaires servent de sauvegarde de partie.
 """
 
 
-info_de_partie = [] # prévisions
+info_de_partie = []
 """
 info_de_partie = [
 	{
 	allowed_channel : channel.name,
 
-	achats_autorisés : [nom_joueur_id, nom_joueur_id],					<= Pour la team n° 0 (implémenté)
+	achats_autorisés : [nom_joueur_id, nom_joueur_id],					<= Pour la team n° 0
 	
 	combat_autorisé : [nom_joueur_id, nom_joueur_id],
 
 	quete en cours : {
 		"titre" : "",
-		"decription" : "",
-		"boss" : "",
-		"players" : [nom_joueur_id, nom_joueur_id, nom_joueur_id]
+		"boss" : {
+			"genre" : "",
+			"race" : "",
+			"classe" : "",
+			"don" : "",
+			"prénom" : "",
+			"pronom" : "",
+			"nom" : "",
+			"age" : "",
+			},
+		"difficulté" : {
+			"difficulté" : "",
+			"nb salles" : x,
+			"récompense" : "",
+			"multiplicateur" : x,
+			},
+		"players" : [nom_joueur_id, nom_joueur_id, nom_joueur_id],
+		"butin" : [],
+		"objetcif" : "",
+		"ennemis" : [
+				{
+				"classe" : "",
+				"race" : "",
+				"don" : "",
+				"force" : X,
+				"constitution" : X,			<= ennemi 0 # A implémenter
+				"dextérité" : X,
+				"intelligence" : X,
+				"sagesse" : X,
+				"charisme" : X,
+				"défense" : X,
+				"PV max" : X,
+				"PV" : X,
+				}
+			]
 		}
 
 	nom_joueur_id : {
@@ -70,70 +102,7 @@ info_de_partie = [
 
 	}
 
-	{
-	allowed_channel : channel.name,
-
-	achats_autorisés : [nom_joueur_id, nom_joueur_id],
 	
-	combat_autorisés : [nom_joueur_id, nom_joueur_id],
-
-	nom_joueur_id : {
-		"p_prénom" : "",
-		"p_nom" : "",
-		"p_pronom" : "",
-		"p_age" : "",
-		"p_race" : "",
-		"p_classe" : "",
-		"p_genre" : "" ,
-		"p_don" : "",													<= Pour la team n° 1 (prévisions)
-		"p_force" : X,
-		"p_constitution" : X,
-		"p_dextérité" : X,
-		"p_intelligence" : X,
-		"p_sagesse" : X,
-		"p_charisme" : X,
-		"p_défense" : X,
-		"p_PV max" : X,
-		"p_PV" : X,
-		"p_PA" : X,
-		"p_XP" : X,
-		"p_sac" : [],
-		"p_équipement" : [],
-		},
-
-		nom_joueur_id : {
-		"p_prénom" : "",
-		"p_nom" : "",
-		"p_pronom" : "",
-		"p_age" : "",
-		"p_race" : "",
-		"p_classe" : "",
-		"p_genre" : "" ,
-		"p_don" : "",
-		"p_force" : X,
-		"p_constitution" : X,
-		"p_dextérité" : X,
-		"p_intelligence" : X,
-		"p_sagesse" : X,
-		"p_charisme" : X,
-		"p_défense" : X,
-		"p_PV max" : X,
-		"p_PV" : X,
-		"p_PA" : X,
-		"p_XP" : X,
-		"p_sac" : [],
-		"p_équipement" : [],
-		},
-	
-
-	"scénario" : 1,
-	"avancement" : 3,
-	"étapes nécessaires" : 5,
-	"inocents tués" : 2,
-	"quête en cours" : "5,3,12,4",
-	"avancement quête" : 2,
-	"étapes quêtes nécessaires" : 3,
-	"code de sauvegarde" : "54,1,14/1,5,78/9"
 	}
 ]
 """
